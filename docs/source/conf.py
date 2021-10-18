efc1400d933b0da9d9cf62606b052faf8c643785
Karl W. Schulz <karl@oden.utexas.edu>
2021-10-18 14:03:54 -0500

    test use of mathbase mdoe instead of imgmath which requires full blown latex install

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,9 +27,9 @@ author = 'Pecos Development Team'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-  'sphinx.ext.imgmath'
-]
+#extensions = [
+#    'sphinx.ext.imgmath',
+#]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
